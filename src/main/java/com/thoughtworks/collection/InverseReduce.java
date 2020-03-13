d7964702fd69ae24777f65abca9f355ad1154ea2
mahuanfy <mahuanfy@foxmail.com>
2020-03-13 14:36:54 +0800

    add inverse reduce.

---
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class InverseReduce {
+
+    private Random random = new Random();
+
+    public InverseReduce() {
+    }
+
+    public InverseReduce(Random random) {
+        this.random = random;
+    }
+
+    public List<Integer> divideToSmaller(int number) {
+        return null;
+    }
+}
